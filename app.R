f0cad9a1cd4ca4c43992af08eee0251823165451
jlomako <71858760+jlomako@users.noreply.github.com>
2022-09-09 15:12:09 -0400

    Update app.R
    changes ui to bootstrap 5

--- app.R
+++ app.R
@@ -20,8 +20,6 @@ library(dplyr)
 library(stringr)
 library(ggplot2)
 library(shiny)
-library(shinydashboard)
-# library(shinyWidgets)
 
 # get data incl death counts and population from repository
 data <- vroom::vroom("https://github.com/jlomako/mortality-canada/raw/main/data/data.csv", show_col_types = FALSE)
@@ -42,28 +40,81 @@ sex <- data %>% distinct(sex)
 age <- data %>% distinct(age)
 year <- data %>% distinct(year)
 
-# shiny app
-ui <- dashboardPage(
-  dashboardHeader(),
-  dashboardSidebar(
-    div(selectInput(inputId = "province", label = "Select a province", choices = provinces, width = "100%")),
-    div(selectInput(inputId = "sex", label = "Select group", choices = sex, width = "100%")),
-    div(selectInput(inputId = "age", label = "Select age", choices = age, width = "100%")),
-    #       div(pickerInput(inputId = "age", label = "Select age", choices = age, multiple = T, selected = "all ages")), # options = list(`actions-box` = TRUE)
-    #       div(pickerInput(inputId = "year", label = "Select year", choices = year, multiple = T, selected = 2021)) # options = list(`actions-box` = TRUE)
-    div(checkboxGroupInput(inputId = "year", label = "Select year",
-                           choices = list("2021" = 2021, "2020" = 2020, "2019" = 2019, "2018" = 2018, 
-                                          "2017" = 2017, "2016" = 2016, "2015" = 2015, "2014" = 2014,
-                                          "2013" = 2013, "2012" = 2012, "2011" = 2011, "2010" = 2010),
-                           selected = 2020:2021))
-  ),
+#########################
+## dashboard interface:
+#########################
+# library(shinydashboard)
+# ui <- dashboardPage(
+#   dashboardHeader(),
+#   dashboardSidebar(
+#     div(selectInput(inputId = "province", label = "Select a province", choices = provinces, width = "100%")),
+#     div(selectInput(inputId = "sex", label = "Select group", choices = sex, width = "100%")),
+#     div(selectInput(inputId = "age", label = "Select age", choices = age, width = "100%")),
+#     #       div(pickerInput(inputId = "age", label = "Select age", choices = age, multiple = T, selected = "all ages")), # options = list(`actions-box` = TRUE)
+#     #       div(pickerInput(inputId = "year", label = "Select year", choices = year, multiple = T, selected = 2021)) # options = list(`actions-box` = TRUE)
+#     div(checkboxGroupInput(inputId = "year", label = "Select year",
+#                            choices = list("2021" = 2021, "2020" = 2020, "2019" = 2019, "2018" = 2018, 
+#                                           "2017" = 2017, "2016" = 2016, "2015" = 2015, "2014" = 2014,
+#                                           "2013" = 2013, "2012" = 2012, "2011" = 2011, "2010" = 2010),
+#                            selected = 2020:2021))
+#   ),
+#   
+#   dashboardBody(
+#     # plot
+#     plotOutput("plot")
+#     
+#   ),
+# )
+
+#########################
+# bootstrap 5 interface
+#########################
+thematic::thematic_shiny(font = "auto")
+ui <- bootstrapPage(
   
-  dashboardBody(
-    # plot
-    plotOutput("plot")
-    
-  ),
-)
+  # uses bootstrap 5
+  theme = bslib::bs_theme(version = 5, bootswatch = "minty"),
+  
+  tags$head(HTML("<title>Mortality rates in Canada</title>")),
+  
+  div(class="container-sm",
+      
+      h1("Mortality in Canada", class="text-center pt-3"),
+      h5("Weekly death counts, by age group and sex", class="text-center pt-0"),
+      
+      # card current occupancy 
+      div(class="row",
+          div(class="col-sm-3",
+              div(class="card",
+                  # div(class="card-header bg-primary", h5("card header left", class="card-title")),
+                  div(class="card-footer px-2", 
+                      div(selectInput(inputId = "province", label = "Select a province", choices = provinces, width = "100%")),
+                      div(selectInput(inputId = "sex", label = "Select group", choices = sex, width = "100%")),
+                      div(selectInput(inputId = "age", label = "Select age", choices = age, width = "100%"))
+                      # div(class="card-footer", h5("card footer right", class="small"))
+                      ),
+                  # div(class="card-footer", h5("card footer left", class="small"))
+                 ),
+          ),
+          div(class="col-sm-9",
+              div(class="card w-100",
+                    #  div(class="card-header bg-primary", h5("card header right", class="card-title")),
+                      div(class="card-body px-0", div(class ="w-100", plotOutput("plot", width = "100%"))),
+                      div(class="card-footer", 
+                          div(checkboxGroupInput(inputId = "year", 
+                                                 label = "Select a year",
+                                                 choices = list("2021" = 2021, "2020" = 2020, "2019" = 2019, "2018" = 2018, 
+                                                 "2017" = 2017, "2016" = 2016, "2015" = 2015, "2014" = 2014,
+                                                 "2013" = 2013, "2012" = 2012, "2011" = 2011, "2010" = 2010),
+                                                 selected = 2020:2021,
+                                                 inline=T))
+                          )
+                    #  div(class="card-footer", h5("card footer right", class="small"))
+                      )    
+              )
+          )
+     )
+  )
 
 
 server <- function(input, output) { 
@@ -79,12 +130,12 @@ server <- function(input, output) {
       geom_line(size = 0.5, na.rm=T) +
       scale_x_date(date_labels = "%b %d", date_breaks = "2 month") +
       # ylim(0,50) +
-      labs(title = "Weekly mortality rate in Canada", 
-           caption = paste(input$province, ":", input$sex, ",", input$age), 
-           y = "mortality rate per 100.000", x = NULL,) +
+      labs(title = "Weekly mortality rate per 100.000 inhabitants", 
+           subtitle = paste0("in ", input$province, ": ", input$sex, ", ", input$age), 
+           y = NULL, x = NULL,) +
       theme_bw() +
       theme(axis.text.x = element_text(angle=90, hjust=0.5, vjust=0.5), 
-            legend.position="top", plot.title = element_text(hjust = 0.5))
+            legend.position="top", plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5))
   }, res = 96, height = "auto")
   
 }
