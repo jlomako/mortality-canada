6262feae601468a87ed93c1a0cc7af58031f4864
jlomako <71858760+jlomako@users.noreply.github.com>
2022-09-09 21:52:56 -0400

    Update app.R
    added google fonts, shifted text around

--- app.R
+++ app.R
@@ -65,6 +65,7 @@ year <- data %>% distinct(year)
 #     
 #   ),
 # )
+###########################
 
 #########################
 # bootstrap 5 interface
@@ -75,19 +76,22 @@ ui <- bootstrapPage(
   # uses bootstrap 5
   theme = bslib::bs_theme(version = 5, bootswatch = "minty"),
   
-  tags$head(HTML("<title>Mortality rates in Canada</title>")),
+  # add google font
+  tags$head(HTML('<title>Mortality rates in Canada</title>
+                  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">')),
   
   div(class="container-sm",
       
-      h1("Mortality in Canada", class="text-center pt-3"),
-      h5("Weekly death counts, by age group and sex", class="text-center pt-0"),
+      h1("Mortality in Canada", class="text-center pt-1"),
+      h5("Weekly deaths, by age group and sex", class="text-center"),
       
       # card current occupancy 
-      div(class="row",
+      div(class="row gx-2",
           div(class="col-sm-3",
-              div(class="card",
+              div(class="card h-100",
                   # div(class="card-header bg-primary", h5("card header left", class="card-title")),
-                  div(class="card-footer px-2", 
+                  div(class="card-body bg-light px-2", 
                       div(selectInput(inputId = "province", label = "Select a province", choices = provinces, width = "100%")),
                       div(selectInput(inputId = "sex", label = "Select group", choices = sex, width = "100%")),
                       div(selectInput(inputId = "age", label = "Select age", choices = age, width = "100%"))
@@ -97,10 +101,10 @@ ui <- bootstrapPage(
                  ),
           ),
           div(class="col-sm-9",
-              div(class="card w-100",
+              div(class="card h-100",
                     #  div(class="card-header bg-primary", h5("card header right", class="card-title")),
-                      div(class="card-body px-0", div(class ="w-100", plotOutput("plot", width = "100%"))),
-                      div(class="card-footer", 
+                      div(class="card-body px-0", div(plotOutput("plot", width = "100%"))),
+                      div(class="card-body bg-light border-top", 
                           div(checkboxGroupInput(inputId = "year", 
                                                  label = "Select a year",
                                                  choices = list("2021" = 2021, "2020" = 2020, "2019" = 2019, "2018" = 2018, 
@@ -108,13 +112,22 @@ ui <- bootstrapPage(
                                                  "2013" = 2013, "2012" = 2012, "2011" = 2011, "2010" = 2010),
                                                  selected = 2020:2021,
                                                  inline=T))
+                          ),
+                      div(class="card-footer bg-white", 
+                          tags$div(
+                        HTML('<p class="text-center fw-light fs-6 lh-sm text-muted">
+                              Data source: Statistics Canada. 
+                              Table 13-10-0768-01  Provisional weekly death counts, by age group and sex <br>
+                              DOI: https://doi.org/10.25318/1310076801-eng<br><br>
+                              © Copyright 2022, jlomako, Montreal
+                             </p>'),
                           )
-                    #  div(class="card-footer", h5("card footer right", class="small"))
-                      )    
-              )
-          )
-     )
-  )
+                        ),
+                      ),    
+              ),
+          ),
+     ),
+)
 
 
 server <- function(input, output) { 
@@ -130,12 +143,13 @@ server <- function(input, output) {
       geom_line(size = 0.5, na.rm=T) +
       scale_x_date(date_labels = "%b %d", date_breaks = "2 month") +
       # ylim(0,50) +
-      labs(title = "Weekly mortality rate per 100.000 inhabitants", 
-           subtitle = paste0("in ", input$province, ": ", input$sex, ", ", input$age), 
+      labs(title = "Mortality rate per 100.000 inhabitants", 
+           subtitle = paste0(input$province, ": ", input$sex, ", ", input$age), 
            y = NULL, x = NULL,) +
       theme_bw() +
-      theme(axis.text.x = element_text(angle=90, hjust=0.5, vjust=0.5), 
-            legend.position="top", plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5))
+      theme(axis.text.x = element_text(angle=90, hjust=0, vjust=0.5), 
+            legend.position="top", plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5),
+            axis.ticks = element_blank(), axis.text = element_text(size = 9))
   }, res = 96, height = "auto")
   
 }
