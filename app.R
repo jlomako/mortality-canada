a3122274de46f255a795e1383f8034690a5199a9
jlomako <71858760+jlomako@users.noreply.github.com>
2022-09-16 15:51:05 -0400

    Update app.R
    added radio buttons to switch from mortality rate to death numbers with y = get(input$type)

--- app.R
+++ app.R
@@ -97,10 +97,13 @@ ui <- bootstrapPage(
                   div(class="card-body bg-light px-2", 
                       div(selectInput(inputId = "province", label = "Select a province", choices = provinces, width = "100%")),
                       div(selectInput(inputId = "sex", label = "Select group", choices = sex, width = "100%")),
-                      div(selectInput(inputId = "age", label = "Select age", choices = age, width = "100%"))
+                      div(selectInput(inputId = "age", label = "Select age", choices = age, width = "100%")),
+                      div(radioButtons(inputId = "type", label = "", 
+                                       choices = c("per 100.000" = "death_rate", "number of deaths" = "deaths"),
+                                       selected = "death_rate", width="100%"))
                       # div(class="card-footer", h5("card footer right", class="small"))
                       ),
-                  # div(class="card-footer", h5("card footer left", class="small"))
+                  # div(class="card-footer")
                  ),
           ),
           div(class="col-sm-9",
@@ -138,16 +141,14 @@ server <- function(input, output) {
   output$plot <- renderPlot({
     selected_province <- reactive(data %>% 
                                     filter(year %in% input$year) %>%
-                                    filter(geo == input$province & 
-                                             sex == input$sex & 
-                                             age == input$age))
+                                    filter(geo == input$province & sex == input$sex & age == input$age))
     selected_province() %>% 
-      ggplot(aes(x = CommonDate, y = death_rate, colour = year)) +
+      ggplot(aes(x = CommonDate, y = get(input$type), colour = year)) +
       geom_line(size = 0.71, na.rm=T) +
       scale_x_date(date_labels = "%b %d", date_breaks = "2 month") +
-      # ylim(0,50) +
       labs(title = "Mortality rate per 100.000 inhabitants", 
            subtitle = paste0(input$province, ": ", input$sex, ", ", input$age), 
+           caption = NULL,
            y = NULL, x = NULL,) +
       theme_bw() +
       theme(axis.text.x = element_text(angle=90, hjust=0, vjust=0.5), 
